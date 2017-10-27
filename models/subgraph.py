7a9d71d56b44b3a7b5f1acd833ea2c61ce242d19
Michael O. Vertolli <michaelvertolli@gmail.com>
2017-10-27 10:00:50 -0400

    added frozen graph handling

--- models/subgraph.py
+++ models/subgraph.py
@@ -1,7 +1,7 @@
 from importlib import import_module
 import os
 import tensorflow as tf
-from errors import SessionGraphError, FirstInitialization
+from errors import SessionGraphError, FirstInitialization, InvalidSubGraphError
 
 
 SUBGRAPH_STR = "Model:\t{}\nInputs:\t{}\nOutputs\t{}\n"
@@ -34,12 +34,23 @@ class SubGraph(object):
                                    ', '.join(self.input_names),
                                    ', '.join(self.output_names))
 
+
+    def restore(self, model_name, config_type, sess, input_map=None):
+        if self.collections:
+            raise InvalidSubGraphError('Base SubGraph cannot have collections. ' +
+                                       'Consider using BuiltSubGraph.')
+        else:
+            tf.import_graph_def(self.frozen_graph_def,
+                                input_map=input_map,
+                                name=model_name)
+
+
 CONFIG_FILE = 'models.{}.config'
 GRAPH_FILE = 'models.{}.graph'
 GRAPH = 'graph_{}'
 META = 'graph_{}.meta'
 DIR = './models/'
-
+CHKPNT = 'checkpoint'
 
 class BuiltSubGraph(SubGraph):
     def __init__(self, model_name, config_type, session, log_dir=None):
@@ -54,6 +65,11 @@ class BuiltSubGraph(SubGraph):
         if session.graph.version != 0:
             raise SessionGraphError('The session graph must be empty.')
         self.log_dir = log_dir
+        if self.log_dir is not None:
+            with open(os.path.join(self.log_dir, CHKPNT)) as f:
+                txt = f.readline()
+            # gets checkpoint file name from log_dir
+            self.log_dir = os.path.join(self.log_dir, txt.split(': ')[1][1:-2])
         if self.graph_is_built(model_name, config_type):
             self.restore(model_name, config_type, session)
             super(BuiltSubGraph, self).__init__(model_name, config_type, session.graph)
@@ -70,6 +86,7 @@ class BuiltSubGraph(SubGraph):
         init = tf.variables_initializer(tf.get_collection('variables'))
         sess.run(init)
         self.saver.save(sess, os.path.join(self.path, GRAPH.format(config_type)), write_state=False)
+
 
     def graph_is_built(self, model_name, config_type):
         module_name = self.get_module_name(model_name)
@@ -92,7 +109,38 @@ class BuiltSubGraph(SubGraph):
             self.saver.restore(sess, os.path.join(self.path, GRAPH.format(config_type)))
         else:
             self.saver = tf.train.Saver(sess.graph.get_collection('variables'))
-            self.saver.restore(sess, log_dir)
+            self.saver.restore(sess, self.log_dir)
 
 
+    def freeze(self, sess):
+        #designed to be called during initialization
+        #sess must include the graph of this subgraph
+        #self.restore(self.name, self.config_type, sess)
+        frozen_graph_def = tf.graph_util.convert_variables_to_constants(
+            sess,
+            sess.graph.as_graph_def(),
+            self.strip_names(self.output_names))
+        #assume this is only ever used to initialize a FrozenSubGraph
+        #so we can set the name parameter to 0
+        return FrozenSubGraph('frozen_{}_0'.format(self.get_module_name(self.name)),
+                              self.config_type, frozen_graph_def)
 
+
+    def strip_names(self, names):
+        return [name.split(':')[0] for name in names]
+
+
+class FrozenSubGraph(SubGraph):
+    def __init__(self, model_name, config_type, frozen_graph_def):
+        super(FrozenSubGraph, self).__init__(model_name, config_type, tf.Graph())
+        self.frozen_graph_def = frozen_graph_def
+
+
+    def restore(self, model_name, config_type, sess, input_map=None):
+        tf.import_graph_def(self.frozen_graph_def,
+                            input_map=input_map,
+                            name=model_name)
+
+
+    def copy(self, new_name):
+        return FrozenSubGraph(new_name, self.config_type, self.frozen_graph_def)
