5e27305dd79923fd49d58d1ea32aad6d701db486
Mike <michaelvertolli@gmail.com>
2017-11-22 08:04:37 -0500

    incomplete nvd_generator_full_reverse

---
+++ models/nvd_cg_ebm_full_reverse/config.py
@@ -0,0 +1,85 @@
+from .. import config_base as cb
+from ..errors import ConfigError
+import numpy as np
+
+#batch_size
+B_16 = 'b16'
+#gamma
+G_5 = 'g0.5'
+G_7 = 'g0.7'
+#lss_type
+BEGAN = 'began'
+SCALED_BEGAN_GMSM = 'scaled_began_gmsm'
+#mdl_type
+Z128_SZ64 = 'z128_sz64'
+Z256_SZ64 = 'z256_sz64'
+Z1024_SZ64 = 'z1024_sz64'
+Z2048_SZ64 = 'z2048_sz64'
+Z256_SZ128 = 'z256_sz128'
+H128 = 'h128'
+H256 = 'h256'
+
+
+def config(type_):
+    config = cb.Config()
+
+
+    if B_16 in type_:
+        config.batch_size = 16
+    else:
+        raise ConfigError('Invalid config type {} for batch_size.'.format(type_))
+    if G_5 in type_:
+        config.gamma = 0.5
+    elif G_7 in type_:
+        config.gamma = 0.7
+    else:
+        raise ConfigError('Invalid config type {} for gamma.'.format(type_))
+    if SCALED_BEGAN_GMSM in type_:
+        config.lss_type = SCALED_BEGAN_GMSM
+    elif BEGAN in type_:
+        config.lss_type = BEGAN
+    else:
+        raise ConfigError('Invalid config type {} for lss_type.'.format(type_))
+    if Z128_SZ64 in type_:
+        config.mdl_type = Z128_SZ64
+        config.z_num = 128
+        config.img_size = 64
+    elif Z256_SZ64 in type_:
+        config.mdl_type = Z256_SZ64
+        config.z_num = 256
+        config.img_size = 64
+    elif Z1024_SZ64 in type_:
+        config.mdl_type = Z1024_SZ64
+        config.z_num = 1024
+        config.img_size = 64
+    elif Z2048_SZ64 in type_:
+        config.mdl_type = Z2048_SZ64
+        config.z_num = 2048
+        config.img_size = 64
+    elif Z256_SZ128 in type_:
+        config.mdl_type = Z256_SZ128
+        config.z_num = 256
+        config.img_size = 128
+    else:
+        raise ConfigError('Invalid config type {} for mdl_type.'.format(type_))
+    if H128 in type_:
+        config.mdl_type = '_'.join([config.mdl_type, H128])
+    elif H256 in type_:
+        config.mdl_type = '_'.join([config.mdl_type, H256])
+    else:
+        raise ConfigError('Invalid config type: {}.'.format(type_))
+
+    config.repeat_num = int(np.log2(config.img_size)) - 2
+    config.base_size = 8
+    config.g_lr = 8e-5
+    config.d_lr = 8e-5
+    config.lr_lower_boundary = 2e-5
+    config.lambda_k = 0.001
+    config.alpha_update_steps = 40000
+    config.alpha_update_step_size = 5000
+
+    config.data_format = 'NCHW'
+
+    config.name = type_
+
+    return config
---
+++ models/nvd_generator_loop/graph.py
@@ -0,0 +1,45 @@
+import numpy as np
+import tensorflow as tf
+from .. import models
+
+
+def build_graph(model_name, config):
+    # alphas = [tf.Variable(0.1, trainable=False, name='alpha'+str(i), dtype=tf.float32)
+    #           for i in range(config.repeat_num-1)]
+    alphas = [tf.placeholder(tf.float32, (), name='alpha'+str(i)) for i in range(config.repeat_num-1)]
+    G_in = tf.placeholder(tf.float32, [None, config.z_num], name='G1_input')
+    G_outs, G_vars = models.GeneratorSkipCNN(G_in,
+                                             config.hidden_num, config.output_num,
+                                             config.repeat_num, alphas,
+                                             config.data_format, False)
+    for alpha in alphas:
+        tf.add_to_collection('inputs', alpha)
+    tf.add_to_collection('inputs', G_in)
+    for i in range(len(G_outs)):
+        temp = tf.identity(G_outs[i], name='G1_output'+str(i))
+        tf.add_to_collection('outputs', temp)
+    for v in G_vars:
+        tf.add_to_collection('G_tensors', v)
+
+    GR_in = tf.identity(G_outs[-1], name='GR_input')
+    GR_out, GR_vars = models.GeneratorRCNN(GR_in,
+                                           config.output_num, config.z_num,
+                                           config.repeat_num, config.hidden_num,
+                                           config.data_format)
+    GR_out = tf.identity(GR_out, name='GR_output')
+    tf.add_to_collection('inputs', GR_in)
+    tf.add_to_collection('outputs', GR_out)
+    for v in GR_vars:
+        tf.add_to_collection('GR_tensors', v)
+    G2_out, G2_vars = models.GeneratorCNN(GR_out,
+                                          config.hidden_num, config.output_num,
+                                          config.repeat_num, config.data_format,
+                                          True)
+    G2_out = tf.identity(G2_out, name='G2_output')
+    tf.add_to_collection('outputs', G2_out)
+    
+    saver = tf.train.Saver()
+    return saver
+
+
+
