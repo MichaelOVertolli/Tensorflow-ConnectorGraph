58a84877bedaa9db51fa3a67e5f82ee986fe3ede
Michael O. Vertolli <michaelvertolli@gmail.com>
2018-04-18 08:41:33 -0400

    new dataset setup code

--- preprocessing.py
+++ preprocessing.py
@@ -15,6 +15,7 @@
 #along with this program.  If not, see http://www.gnu.org/licenses/
 ###############################################################################
 
+from evaluations import flatten2color
 from glob import glob
 import json
 from nltk.corpus import wordnet as wn
@@ -140,6 +141,25 @@ def crop_resize(imgs_dir, out_dir):
         im.close()
 
 
+def crop_resize2(imgs_dir, out_dir, crop_box, resize):
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    files = os.listdir(imgs_dir)
+    for f in files:
+        im = Image.open(os.path.join(imgs_dir, f))
+        h, w = im.size
+        if h > w:
+            crop = im.crop(crop_box)
+        else:
+            crop = im.crop(crop_box)
+        if resize is not None:
+            resized = crop.resize(resize, Image.BICUBIC)
+        else:
+            resized = crop
+        resized.save(os.path.join(out_dir, f))
+        im.close()
+
+
 def mscelebmain():
     crop_resize('/home/olias/data/msceleb/MsCeleb', '/home/olias/data/msceleb/splits/train')
 
@@ -170,44 +190,93 @@ def dataset_resize(imgs_dir, new_dir, new_size):
         im.close()
 
 
-def mnist_to_imgs(new_dir, base_size=32):
-    if not os.path.exists(new_dir):
-        os.makedirs(new_dir)
+def celeb_to_imgs(new_dir, imgs_dir='/home/olias/data/img_align_celeba/', train=True, all_dir=None):
+    if all_dir is None:
+        dirs = [new_dir]
+    else:
+        dirs = [new_dir, all_dir]
+    for dr in dirs:
+        if not os.path.exists(dr):
+            os.makedirs(dr)
+    TRAIN_STOP = 162770
+    NUM_EXAMPLES = 202599
+    CROP_BOX = [25, 50, 128+25, 50+128]
+    if train:
+        start, stop = 0, TRAIN_STOP
+    else:
+        start, stop = TRAIN_STOP, NUM_EXAMPLES # collapsedd validation to test
+    
+    files = ['{:06}.jpg'.format(i+1) for i in range(start, stop)]
+    
+    for f in files:
+        im = Image.open(os.path.join(imgs_dir, f))
+        crop = im.crop(CROP_BOX)
+        for dr in dirs:
+            crop.save(os.path.join(dr, f))
+        im.close()
+
+
+def mnist_to_imgs(new_dir, base_size=32, train=True, all_dir=None):
+    if all_dir is None:
+        dirs = [new_dir]
+    else:
+        dirs = [new_dir, all_dir]
+    for dr in dirs:
+        if not os.path.exists(dr):
+            os.makedirs(dr)
     mnist = tf.contrib.learn.datasets.load_dataset('mnist')
-    images = mnist.train.images
+    if train:
+        images = mnist.train.images
+        labels = mnist.train.labels
+        base_index = 0
+    else:
+        images = mnist.test.images
+        labels = mnist.test.labels
+        base_index = 55000
+    labels = np.asarray(labels, dtype=np.int32)
     for i in range(images.shape[0]):
         arr = np.reshape(images[i, :], [28, 28])
         im = Image.fromarray(arr*255, 'I').convert('RGB')
         o_im = im.resize([base_size, base_size], Image.NEAREST)
-        o_im.save(os.path.join(new_dir, '{:05}.jpg'.format(i)))
+        label = labels[i]
+        for dr in dirs:
+            o_im.save(os.path.join(dr, '{}_{:05}.jpg'.format(label, base_index+i)))
 
 
-def cifar10_to_imgs(new_dir):
-    files = glob('/home/olias/data/cifar-10-batches-bin/data_batch*.bin')
-    
-    if not os.path.exists(new_dir):
-        os.makedirs(new_dir)
+def cifar10_to_imgs(new_dir, train=True, all_dir=None):
+    if all_dir is None:
+        dirs = [new_dir]
+    else:
+        dirs = [new_dir, all_dir]
+    for dr in dirs:
+        if not os.path.exists(dr):
+            os.makedirs(dr)
+    if train:
+        files = glob('/home/olias/data/cifar-10-batches-bin/data_batch*.bin')
+        count = 0
+    else:
+        files = ['/home/olias/data/cifar-10-batches-bin/test_batch.bin']
+        count = 50000
 
     label_bytes = 1
     h, w, c = 32, 32, 3
     image_bytes = h * w * c
 
-    convert = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-    counts = dict([(s, 0) for s in convert])
-    convert = dict([(i, s) for i, s in enumerate(convert)])
+    # convert = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+    # convert = dict([(i, s) for i, s in enumerate(convert)])
 
     for f in files:
         with open(f, 'rb') as stream:
             label_i = stream.read(label_bytes)
             while label_i != '':
                 label_i = np.frombuffer(label_i, dtype=np.uint8)[0]
-                label = convert[label_i]
-                index = counts[label]
-                counts[label] += 1
+                # label = convert[label_i]
                 img = np.frombuffer(stream.read(image_bytes), dtype=np.int8)
                 img = np.transpose(np.reshape(img, [c, w, h]), [1, 2, 0]).astype(np.uint8)
                 img = Image.fromarray(img)
-                img.save(os.path.join(new_dir, '{}_{}.jpg'.format(label, index)))
+                for dr in dirs:
+                    img.save(os.path.join(dr, '{}_{:05}.jpg'.format(label_i, count)))
+                count += 1
                 label_i = stream.read(label_bytes)
 
 
@@ -219,7 +288,7 @@ def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 
-def to_tfrecord(fname, new_dir, img_dir, shape, shards=25): # assumes shape is [h, w, c]
+def to_tfrecord_shard(fname, new_dir, img_dir, shape, shards=25): # assumes shape is [h, w, c]
     if not os.path.exists(new_dir):
         os.makedirs(new_dir)
     with open(os.path.join(new_dir, 'img_shape.json'), 'w') as f:
@@ -246,6 +315,29 @@ def to_tfrecord(fname, new_dir, img_dir, shape, shards=25): # assumes shape is [
         example = tf.train.Example(features=tf.train.Features(feature={
             'index': _int64_feature(i),
             'image_raw': _bytes_feature(im_raw)}))
+
+        writer.write(example.SerializeToString())
+        
+    writer.close()
+
+
+def to_tfrecord(fname, new_dir, img_dir, shape, shuffle=False): # assumes shape is [h, w, c]
+    if not os.path.exists(new_dir):
+        os.makedirs(new_dir)
+    with open(os.path.join(new_dir, 'img_shape.json'), 'w') as f:
+        f.write(json.dumps(shape))
+    imgs = glob(os.path.join(img_dir, '*.jpg'))
+    if shuffle:
+        shuffle(imgs)
+
+    writer = tf.python_io.TFRecordWriter(os.path.join(new_dir, fname+'.tfrecords'))
+    for i, img in enumerate(imgs):
+        with open(img, 'rb') as f:
+            im_raw = f.read()
+
+        example = tf.train.Example(features=tf.train.Features(feature={
+            'image_raw': _bytes_feature(im_raw),
+            'image_name': _bytes_feature(os.path.split(img)[1])}))
 
         writer.write(example.SerializeToString())
         
@@ -288,3 +380,16 @@ def prep_param_csv(fname, start_index, end_index=14, nan_val=-.1, log=True):
             ar[ar > 9.] = 0.
     ar[np.isnan(ar)] = nan_val
     np.savetxt(os.path.join(path, 'prepped_'+tail), ar, fmt='%.4f', delimiter=',', header=header[:-1], comments='')
+
+
+def get_all_colors(log_dir, size=[32, 32], quantize=4, all_colors=set([])):
+    files = os.listdir(log_dir)
+    for f in files:
+        im = Image.open(os.path.join(log_dir, f))
+        if size is not None:
+            im = im.resize(size)
+        im = np.array(im)
+        if quantize is not None:
+            im = (im / quantize) * quantize
+        all_colors |= set([tuple(c) for c in flatten2color(im).tolist()])
+    return all_colors
